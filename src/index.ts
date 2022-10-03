ec80eb366d0d9336714f923dd260fa03e294d2e2
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2022-10-03 16:16:22 +0200

    feat: add js api

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,40 @@
-#!/usr/bin/env node
-
 import { lstat, readdir, rmdir, stat, unlink } from 'node:fs/promises'
 import { join } from 'node:path'
 import files from '@unvented/files'
-import minimist from 'minimist'
 
-(async () => {
-  const argv = minimist(process.argv.slice(2))
+const stats = async (path: string) => {
+  try {
+    const s = await stat(path)
 
-  const stats = async (path: string) => {
-    try {
-      const s = await stat(path)
-  
-      return s
-    } catch (err) {
-      return undefined
-    }
+    return s
+  } catch (err) {
+    return undefined
+  }
+}
+
+, removeEmptyDirectories = async (directory: string) => {
+  const stats = await lstat(directory)
+
+  if (!stats.isDirectory()) return
+
+  let fileNames = await readdir(directory)
+
+  if (fileNames.length > 0) {
+    const recursiveRemovalPromises = fileNames.map(
+      fileName => removeEmptyDirectories(join(directory, fileName))
+    )
+
+    await Promise.all(recursiveRemovalPromises)
+
+    fileNames = await readdir(directory)
   }
 
-  const removeEmptyDirectories = async (directory: string) => {
-    const stats = await lstat(directory)
+  if (fileNames.length === 0)
+    await rmdir(directory)
+}
 
-    if (!stats.isDirectory()) return
-
-    let fileNames = await readdir(directory)
-
-    if (fileNames.length > 0) {
-      const recursiveRemovalPromises = fileNames.map(
-        fileName => removeEmptyDirectories(join(directory, fileName))
-      )
-
-      await Promise.all(recursiveRemovalPromises)
-
-      fileNames = await readdir(directory)
-    }
-
-    if (fileNames.length === 0)
-      await rmdir(directory)
-  }
-
-  for (let i of argv._) {
-    i = join(process.cwd(), i.startsWith('./') || i.startsWith('../') ? i : `./${i}`)
-
+export const deleteFiles = async (...entries: string[]) => {
+  for (let i of entries) {
     const s = await stats(i)
 
     if (!s) continue
@@ -55,4 +48,4 @@ import minimist from 'minimist'
       await removeEmptyDirectories(i)
     }
   }
-})()
+}
---
+++ types/index.d.ts
@@ -0,0 +1,1 @@
+export declare const deleteFiles: (...entries: string[]) => Promise<void>
